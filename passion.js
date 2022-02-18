e619e6c28225fc37f0cace3efc56198d0e1fbf90
Mariyah Mahmood <mari80rb@stud.kea.dk>
2022-02-18 09:26:20 +0100

    json

--- passion.js
+++ passion.js
@@ -24,5 +24,25 @@ btn.addEventListener("click", toggleMenu);
 /******************* BURGERMENU SLUT ******************/
 
 const url = "https://tema7-64ef.restdb.io/rest/planter";
-const key = "600ec2fb1346a1524ff12de4";
-let data = [];
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("id");
+let plante;
+
+const myHeaders = {
+  "x-apikey": "600ec2fb1346a1524ff12de4"
+}
+
+document.addEventListener("DOMContentLoaded", loadJSON);
+console.log("ID", id)
+
+async function loadJSON(){
+    const JSONData = await fetch (`https://tema7-64ef.restdb.io/rest/planter/${id}`, {
+        headers: myHeaders
+        
+    });
+
+    plante = await JSONData.json();
+
+    console.log("Planter", plante);
+    visPlante(plante);
+}
